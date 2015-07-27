b2af0c253dc3018e19ebc4afc47382020e3456bf
ovidia <molnarovidia@yahoo.com>
2015-07-27 13:53:36 +0300

    Final version
    Final version of Number Ordering application.

--- Number_Ordering/js/NumbersApplicationScript.js
+++ Number_Ordering/js/NumbersApplicationScript.js
@@ -1,11 +1,15 @@
 var unsortedNumbers = [234,132,12, 400];
 var sortedNumbers = [12,132,234,400];
+var letter = ["F", "I", "V", "E"];
 var dropTargetDiv1 = '';
 var dropTargetDiv2 = '';
-dropTargetDiv1 = "<div ondrop = 'drop(event)' ondragover = 'allowDrop(event)' class = 'drop-target1' id = 'drtarget1' >";
-dropTargetDiv2 = "<div ondrop = 'drop(event)' ondragover = 'allowDrop(event)' class = 'drop-target2' id = 'drtarget2'>";
 
-function createNumberedLabels()
+/*wrappers for numbered and empty elements */
+dropTargetDiv1 = "<li><div ondrop = 'drop(event)' ondragover = 'allowDrop(event)' class = 'drop-target1 list-group-item panel img-responsive' id = 'drtarget1' >";
+dropTargetDiv2 = "<li><div class = 'drop-target2 list-group-item panel img-responsive ' id = 'drtarget2'>";
+
+
+function createNumberedElements()
 {
     var div = '';
     var image = '';
@@ -14,17 +18,17 @@ function createNumberedLabels()
     
     for (var i = 0; i < unsortedNumbers.length; i++) 
     { 
-        div += "<div draggable = 'true' ondragstart = 'drag(event)' ondragover = 'allowDrop(event)' id = 'drag" + i + "' class = 'drag'>";
-        image = "<img src = '../img/starr.png' class = 'img1' draggable = 'false'><br>";
+        div += "<div draggable = 'true' ondragstart = 'drag(event)'  id = 'drag" + i + "' class = 'drag panel img-responsive'>";
+        image = "<img src = '../img/starr.png' class = 'img1 img-rounded img-responsive' draggable = 'false'><br>";
         label = "<label class = 'number-label'> "+ unsortedNumbers[i] + " </label></div>";
         div += image + label;                          
     } 
-    
-    dropTargetDiv1 += div + "</div>";
+
+    dropTargetDiv1 += div + "</div></li>";
     list.append(dropTargetDiv1);
 }
 
-function createEmptyLabels()
+function createEmptyElements()
 {
     var div = '';
     var image = '';
@@ -33,13 +37,13 @@ function createEmptyLabels()
     
     for (var i = 0; i < unsortedNumbers.length; i++) 
     { 
-        div += "<div draggable = 'false'  id = 'drop" + i + "' class = 'drop'>";
-        image = "<img src = '../img/starr.png' class = 'img1' draggable = 'false'><br>";
+        div += "<div draggable = 'false'  id = 'drop" + i + "' ondrop = 'drop(event)' ondragover = 'allowDrop(event)' class = 'drop panel img-responsive'>";
+        image = "<img src = '../img/starr.png' class = 'img1 img-rounded img-responsive' draggable = 'false'><br>";
         label = "<label class = 'number-label'> </label></div>";
         div += image + label;                    
     } 
 
-    dropTargetDiv2 += div + "</div>";   
+    dropTargetDiv2 += div + "</div></li>";   
     list.append(dropTargetDiv2); 
 }
 
@@ -53,76 +57,92 @@ function drag(ev)
 {
     ev.dataTransfer.setData("text", ev.target.id);
     var data = ev.dataTransfer.getData("text");
-    $('#'+ data).removeClass('correct'); 
-    $('#drop').removeClass('correct'); 
-    $('#'+ data).removeClass('incorrect'); 
-    $('#drop').removeClass('incorrect');
+
+    var i = (ev.target.id).substr((ev.target.id).length-1, (ev.target.id).length);
+    $('#'+ data).removeClass('drop-drtarget1'); 
+    $("#" + data + " .img1").css({"opacity":"0.2"});
 }
 
 function drop(ev) 
 {
     ev.preventDefault();    
-    var data = ev.dataTransfer.getData("text");    
-    ev.target.appendChild(document.getElementById(data));
-
-    for(var i = 0; i < unsortedNumbers.length; i++)
+    var data = ev.dataTransfer.getData("text");
+      
+    if((ev.target.className) != 'img1 img-rounded img-responsive') 
     {
-        if ( $.browser.webkit ) 
+        ev.target.appendChild(document.getElementById(data));
+
+        if((ev.target.id) == 'drtarget1') 
         {
-            if((ev.target.id) == 'drop'+ i )
+            correctAnswers--;
+            if(correctAnswers <= 0)
+                correctAnswers = unsortedNumbers.length - incorrectAnswers.length;
+        }    
+
+        for(var i = 0; i < unsortedNumbers.length; i++)
+        {
+            if ( $.browser.webkit ) 
             {
-                $('#'+ data).css({"margin-top": "-55px", "margin-left": '-3px'});
-                $("#" + data + " .img1").css({"opacity":"1"});             
-                $(ev.target).addClass('swing');
-                getAnswer(data, i );
-                showMessage();
-                i = unsortedNumbers.length;
+                if((ev.target.id) == 'drop'+ i )
+                {       
+                    getAnswer(data, i );
+                    $(ev.target).addClass('swing');
+                    showMessage();
+                    i = unsortedNumbers.length+1;
+                }
+
+                if((ev.target.id) == 'drtarget1')
+                {  
+                    $('#'+ data).removeClass('incorrect');
+                    $('#'+data).addClass('drop-drtarget1'); 
+                }
             }
             else
-            {
-                $('#'+data).css({"margin-top": "27px", "margin-left": "10px"}); 
-            }
-        }
-        if ( $.browser.mozilla ) 
-        {
-            if((ev.target.id) == 'drop'+i)
-            {                
-                $('#'+data).css({"margin-top": "-59px", "margin-left": '-3px'});                
-                $("#" + data + " .img1").css({"opacity":"1"}); 
-                $(ev.target).addClass('swing');
-                getAnswer(data, i );
-                showMessage();
-                i = unsortedNumbers.length;
-            }
-            else
-            {
-                $('#'+ data).css({"margin-top": "27px", "margin-left": "10px"}); 
-            }
-        }
-    }     
+                if ( $.browser.mozilla ) 
+                {
+                    if((ev.target.id) == 'drop'+i)
+                    {                        
+                        getAnswer(data, i );
+                        $(ev.target).addClass('swing');
+                        showMessage();
+                        i = unsortedNumbers.length+1;
+                    }
+
+                    if((ev.target.id) == 'drtarget1')
+                    {
+                        $('#'+ data).removeClass('incorrect');
+                        $('#'+ data).addClass('drop-drtarget1'); 
+                    }
+                }        
+        }  
+    }   
 }
 
 var correctAnswers = 0;
 var incorrectAnswers ="";
+
+/* checks user's answer and counts the right and wrong answers*/
 function getAnswer(data, target)
 {
     var number = -1;
     number = $('#'+data).text();
 
-   for(var i = 0; i < sortedNumbers.length ; i++)
+    for(var i = 0; i < sortedNumbers.length ; i++)
         if (sortedNumbers[target] == number)
         {
+            $('#'+ data).addClass('correct');
+            $('#'+ data).attr("draggable", 'false');
+            $("#" + data + " .img1").css({"opacity":"1"});
+            $('#'+ data + ' .number-label').append("<br><label class = 'letter-label'><center>" + letter[target] + "</center></label>");
             correctAnswers++;
-            $('#'+data).addClass('correct');
-            $('.drop#drop'+i).addClass('correct');
             i = sortedNumbers.length;
         } 
         else 
         {
             $('#'+data).addClass('incorrect');
-            $('.drop#drop'+i).addClass('incorrect');
             incorrectAnswers += target;
             i = sortedNumbers.length;
+
         }
 }
 
@@ -130,16 +150,28 @@ function showMessage()
 {
     if(correctAnswers == sortedNumbers.length) 
     {
-        $('#bubble-message').append("We sliped on the slied 10 times!");
+        $('.bubble #bubble-message').empty();
+        $('#sad').hide();
+        $('.bubble').append("<center><label id = 'bubble-message'> We sliped on the slide 5 times!</label></center>");
         $('.bubble').show();
         $('#happy').show();
     }
     else 
         if($('#drtarget1').is(':empty') == true)
         {
-            $('#bubble-message').append("Try again!");
+            $('.bubble #bubble-message').empty();
             $('#sad').show();
+            $('#happy').hide();
+            $('.bubble').append("<center><label id = 'bubble-message'>Try again!</label></center>");
+            $('.bubble').show();            
         }
+
+    $('.form-group').click(function () 
+    {
+        $('.bubble').hide();
+        $('#happy').hide();
+        $('#sad').hide();
+    });
 }
 
 function init()
@@ -167,13 +199,12 @@ function init()
     {
         $(this).removeClass('active');
     });
-    var correctAnswers = 0;
-    var incorrectAnswers ="";
 }
 
 $(document).ready(function() 
 { 
-    createNumberedLabels();
-    createEmptyLabels();
+    createNumberedElements();
+    createEmptyElements();
     init();
 });
+
